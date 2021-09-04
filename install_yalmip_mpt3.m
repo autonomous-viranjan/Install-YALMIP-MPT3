eca7fa25f27b1d53677bd0e473f05926d3aedca3
Viranjan Bhattacharyya <62226470+autonomous-viranjan@users.noreply.github.com>
2021-09-04 15:57:54 -0400

    Add files via upload

---
+++ install_yalmip_mpt3.m
@@ -0,0 +1,15 @@
+%% YALMIP
+cd C:\Users\viran\Documents\MATLAB\YALMIP
+urlwrite('https://github.com/yalmip/yalmip/archive/master.zip','yalmip.zip');
+unzip('yalmip.zip','yalmip')
+addpath(genpath([pwd filesep 'yalmip']));
+savepath
+%% MPT3
+mkdir('tbxmanager')
+cd tbxmanager
+urlwrite('http://www.tbxmanager.com/tbxmanager.m', 'tbxmanager.m');
+tbxmanager
+savepath
+tbxmanager install mpt mptdoc cddmex fourier glpkmex hysdel lcp yalmip sedumi espresso
+tbxmanager restorepath
+mpt_init
